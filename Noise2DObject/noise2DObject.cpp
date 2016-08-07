05ad22597c193bc97027e4148d3eaad407113526
Alex <alxpla123@gmail.com>
2016-08-07 13:38:55 +0200

    Corrected license
    corrected license copyright

--- Noise2DObject/noise2DObject.cpp
+++ Noise2DObject/noise2DObject.cpp
@@ -1,6 +1,6 @@
 //-----------------------------------------------------------------------------
 // Copyright (c) 2016 Alex Piola
-// Some parts are Copyright (c) 2012 GarageGames, LLC
+// Portions Copyright (c) 2012 GarageGames, LLC
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
